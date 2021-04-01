2d0838df178a4fb20707515c781da8ed4acb15cb
Mark E. Haase <mehaase@gmail.com>
2021-03-31 21:40:02 -0400

    Finish setup.py so that it installs kernel

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
+# This is loosely adapted from ipykernel's setup.py
 
-import sys
 from glob import glob
 import os
 import shutil
+import sys
 
 from setuptools import setup
+
+
+DISPLAY_NAME = "Python 3 Trio"
+KERNEL_NAME = "python3-trio"
 
 
 setup_args = dict(
@@ -15,47 +20,52 @@ setup_args = dict(
     long_description="Installs a kernelspec for Jupyter Lab/Notebook based on ipykernel with Trio mode enabled",
     packages=["trio_jupyter"],
     package_data={
-        'ipykernel': ['resources/*.*'],
+        "trio_jupyter": ["resources/*.*"],
     },
-    author='Mark E. Haase',
-    author_email='mehaase@gmail.com',
-    url='https://github.com/mehaase/trio-jupyter',
-    license='BSD',
-    python_requires='>=3.7',
+    author="Mark E. Haase",
+    author_email="mehaase@gmail.com",
+    url="https://github.com/mehaase/trio-jupyter",
+    license="BSD",
+    python_requires=">=3.7",
     install_requires=[
-        'ipykernel>=5.5.3',
+        "ipykernel>=5.5.0",
+        "trio>=0.18.0",
     ],
     classifiers=[
-        'Framework :: Trio',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
+        "Framework :: Trio",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
     ],
 )
 
 
-if any(a.startswith(('bdist', 'install')) for a in sys.argv):
+if any(arg.startswith(("bdist", "install")) for arg in sys.argv):
+    here = os.path.abspath(os.path.dirname(__file__))
     sys.path.insert(0, here)
-    from ipykernel.kernelspec import write_kernel_spec, make_ipkernel_cmd, KERNEL_NAME
+    from ipykernel.kernelspec import write_kernel_spec, make_ipkernel_cmd
+    extra_arguments = ["--IPKernelApp.trio_loop=True"]
 
-    # When building a wheel, the executable specified in the kernelspec is simply 'python'.
-    if any(a.startswith('bdist') for a in sys.argv):
-        argv = make_ipkernel_cmd(executable='python')
+    # When building a wheel, the executable specified in the kernelspec is simply "python".
+    if any(arg.startswith("bdist") for arg in sys.argv):
+        argv = make_ipkernel_cmd(executable="python", extra_arguments=extra_arguments)
     # When installing from source, the full `sys.executable` can be used.
-    if any(a.startswith('install') for a in sys.argv):
-        argv = make_ipkernel_cmd()
-    dest = os.path.join(here, 'data_kernelspec')
+    if any(arg.startswith("install") for arg in sys.argv):
+        argv = make_ipkernel_cmd(extra_arguments=extra_arguments)
+    dest = os.path.join(here, "data_kernelspec")
     if os.path.exists(dest):
         shutil.rmtree(dest)
-    write_kernel_spec(dest, overrides={'argv': argv})
+    write_kernel_spec(dest, overrides={"argv": argv, "display_name": DISPLAY_NAME})
+    resource_path = os.path.join(os.path.dirname(__file__), "trio_jupyter", "resources")
+    shutil.copytree(resource_path, dest, dirs_exist_ok=True)
 
-    setup_args['data_files'] = [
+    setup_args["data_files"] = [
         (
-            pjoin('share', 'jupyter', 'kernels', KERNEL_NAME),
-            glob(pjoin('data_kernelspec', '*')),
+            os.path.join("share", "jupyter", "kernels", KERNEL_NAME),
+            glob(os.path.join("data_kernelspec", "*")),
         )
     ]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup(**setup_args)
